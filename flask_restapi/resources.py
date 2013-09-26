38dfd00eaeea7e435c8f019090ea9264c90374e4
saml <skynare@gmail.com>
2013-09-26 16:54:25 -0400

    rename

--- flask_restapi/resources.py
+++ flask_restapi/resources.py
@@ -7,7 +7,24 @@ from flask.views import MethodView
 def asjson(resource):
     return Response(json.dumps(resource.repr_hal()), mimetype='application/json')
 
+class Resource(object):
+    dispatch = {}
+    def method(self, method_name):
+
+        self.dispatch[method_name][content_type][accept]
+    def __call__(self, **args, **kwargs):
+        pass
+
+    def dispatch_request(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+
+
 class Index(MethodView):
+    @resource.method('GET')
+    @resource.content_type('application/json')
+    @resource.accept('application/json')
     def get(self):
         return 'hello: ' + url_for('.index', _external=True)
 
