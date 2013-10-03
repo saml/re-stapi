2d3060be0d5256c39d124a97278b11d79174f27c
saml <skynare@gmail.com>
2013-10-03 13:26:12 -0400

    asdf

--- restapiexample/views/__init__.py
+++ restapiexample/views/__init__.py
@@ -1,1 +1,10 @@
 __author__ = 'saml'
+
+from flask import make_response
+
+def response_as(body_str, mimetype='application/json', charset='utf-8'):
+    resp = make_response(body_str)
+    content_type = '{0}; charset={1}'.format(mimetype, charset)
+    resp.headers['Content-Type'] = content_type
+    return resp
+
